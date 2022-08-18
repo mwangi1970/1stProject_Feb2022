65744b0fb6ff1b75ac3b8c1c7c2e3abfad380ad5
mwangi1970 <alltimeawesomedeals@gmail.com>
2022-08-18 16:00:41 -0400

    first commit

---
+++ src/variousConcepts/LaunchChrome.java
@@ -0,0 +1,19 @@
+package variousConcepts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mugor\\Feb22_selenium\\1st_selenium\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.close();
+		
+	}
+
+}
